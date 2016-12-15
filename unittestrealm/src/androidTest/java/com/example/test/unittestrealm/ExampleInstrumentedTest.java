64bf3971f2af88e26702a2484aece75077b0343f
harry <harry@rideaweming.com>
2016-12-15 16:26:25 +0900

    add unittest for realm

--- unittestrealm/src/androidTest/java/com/example/test/unittestrealm/ExampleInstrumentedTest.java
+++ unittestrealm/src/androidTest/java/com/example/test/unittestrealm/ExampleInstrumentedTest.java
@@ -2,8 +2,14 @@ package com.example.test.unittestrealm;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.Espresso;
+import android.support.test.espresso.ViewAssertion;
+import android.support.test.espresso.assertion.ViewAssertions;
+import android.support.test.espresso.matcher.ViewMatchers;
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -16,6 +22,10 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mainActivityActivityTestRule = new ActivityTestRule<MainActivity>(MainActivity.class);
+
     @Test
     public void useAppContext() throws Exception {
         // Context of the app under test.
@@ -23,4 +33,9 @@ public class ExampleInstrumentedTest {
 
         assertEquals("com.example.test.unittestrealm", appContext.getPackageName());
     }
+
+    public void testShouldBeAbleToLaunchActivityAndSeeRealmResults(){
+        Espresso.onView(ViewMatchers.withText("John Senior got older: 89")).check(ViewAssertions.matches(ViewMatchers.isDisplayed()));
+    }
+
 }
