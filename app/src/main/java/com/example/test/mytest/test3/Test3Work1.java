9511b20a4375885ba32616b44bd65e4828862df0
hongken <koreaken01@gmail.com>
2017-04-01 13:43:10 +0900

    test3>renaming - renamed from test3Work1 to Test3Work1

--- app/src/main/java/com/example/test/mytest/test3/Test3Work1.java
+++ app/src/main/java/com/example/test/mytest/test3/Test3Work1.java
@@ -4,7 +4,7 @@ package com.example.test.mytest.test3;
  * Created by Harry on 2017. 4. 1..
  */
 
-public class test3Work1 {
+public class Test3Work1 {
     private int num;
     private int num1;
 }
